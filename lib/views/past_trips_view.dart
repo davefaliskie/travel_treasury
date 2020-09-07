16ea82850f59604692ef4b4f1bd264515a6a9fc4
Dave Faliskie <davefaliskie@gmail.com>
2020-09-07 17:43:51 -0500

    add search to past trips

--- lib/views/past_trips_view.dart
+++ lib/views/past_trips_view.dart
@@ -1,38 +1,103 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_budget/models/Trip.dart';
 import 'package:travel_budget/widgets/provider_widget.dart';
 import 'package:travel_budget/widgets/trip_card.dart';
 
 
-class PastTripsView extends StatelessWidget {
-  Stream<QuerySnapshot> getUsersPastTripsStreamSnapshots(
-      BuildContext context) async* {
+class PastTripsView extends StatefulWidget {
+  @override
+  _PastTripsViewState createState() => _PastTripsViewState();
+}
+
+class _PastTripsViewState extends State<PastTripsView> {
+  TextEditingController _searchController = TextEditingController();
+
+  Future resultsLoaded;
+  List _allResults = [];
+  List _resultsList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    resultsLoaded = getUsersPastTripsStreamSnapshots();
+  }
+
+
+  _onSearchChanged() {
+    searchResultsList();
+  }
+
+  searchResultsList() {
+    var showResults = [];
+
+    if(_searchController.text != "") {
+      for(var tripSnapshot in _allResults){
+        var title = Trip.fromSnapshot(tripSnapshot).title.toLowerCase();
+
+        if(title.contains(_searchController.text.toLowerCase())) {
+          showResults.add(tripSnapshot);
+        }
+      }
+
+    } else {
+      showResults = List.from(_allResults);
+    }
+    setState(() {
+      _resultsList = showResults;
+    });
+  }
+
+  getUsersPastTripsStreamSnapshots() async {
     final uid = await Provider.of(context).auth.getCurrentUID();
-    yield* Firestore.instance
+    var data = await Firestore.instance
         .collection('userData')
         .document(uid)
         .collection('trips')
         .where("endDate", isLessThanOrEqualTo: DateTime.now())
         .orderBy('endDate')
-        .snapshots();
+        .getDocuments();
+    setState(() {
+      _allResults = data.documents;
+    });
+    searchResultsList();
+    return "complete";
   }
-
 
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
           Text("Past Trips", style: TextStyle(fontSize: 20)),
+          Padding(
+            padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 30.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search)
+              ),
+            ),
+          ),
           Expanded(
-            child: StreamBuilder(
-                stream: getUsersPastTripsStreamSnapshots(context),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) return const Text("Loading...");
-                  return new ListView.builder(
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (BuildContext context, int index) =>
-                          buildTripCard(context, snapshot.data.documents[index]));
-                }),
+            child: ListView.builder(
+                itemCount: _resultsList.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    buildTripCard(context, _resultsList[index]),
+            )
+
           ),
         ],
       ),
