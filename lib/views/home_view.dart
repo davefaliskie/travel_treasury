3c605b876773f1d308f91ea78f53de4c2b2349db
Dave Faliskie <davefaliskie@gmail.com>
2021-01-23 15:26:01 -0600

    use stack to build trip detail card ontop of image

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_budget/models/Trip.dart';
 import 'package:travel_budget/views/home_widgets/home_header.dart';
+import 'package:travel_budget/views/home_widgets/trip_details_card.dart';
 
 
 class HomeView extends StatefulWidget {
@@ -22,6 +23,7 @@ class _HomeViewState extends State<HomeView> {
       child: Column(
         children: [
           homeHeader(context, widget.trip),
+          tripDetailsCard(context, widget.trip),
         ],
       ),
     );
---
+++ lib/views/home_widgets/trip_details_card.dart
@@ -0,0 +1,77 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:travel_budget/models/Trip.dart';
+
+Widget tripDetailsCard(BuildContext context, Trip trip) {
+  return Container(
+    child: Stack(
+      overflow: Overflow.visible,
+      alignment: Alignment.bottomCenter,
+      children: [
+        Container(
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 250,
+                child: trip.getLocationImage(),
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          top: 150,
+          left: 15,
+          right: 15,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          child: AutoSizeText(
+                            trip.title,
+                            style: TextStyle(fontSize: 30.0),
+                            maxLines: 2,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                            "${DateFormat('MMM dd, yyyy').format(trip.startDate).toString()} - ${DateFormat('MMM dd, yyyy').format(trip.endDate).toString()}"),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("\$${(trip.budget.floor() * trip.getTotalTripDays()).toString()}",
+                          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0, left: 4.0),
+                        child: Text("total budget"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
